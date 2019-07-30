c54a0cefadd22edce5ba160923811a2021e50f50
Carl Henningsson <carl@beevelopment.com>
2019-07-30 18:03:11 +0200

    Commit One

---
+++ compoundads/Controller/PersonalizeController.swift
@@ -0,0 +1,30 @@
+//
+//  PersonalizeController.swift
+//  compoundads
+//
+//  Created by Carl Henningsson on 2019-05-25.
+//  Copyright © 2019 Carl Henningsson. All rights reserved.
+//
+
+import UIKit
+
+class PersonalizeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
